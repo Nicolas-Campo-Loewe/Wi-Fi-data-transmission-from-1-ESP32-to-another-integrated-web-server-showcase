a452644c96ed8c6b52cfd70b0ff72849770e5bfc
Nicolas-Campo-Loewe <n.campoloewe@gmail.com>
2025-12-23 19:35:21 +0100

    Update client.ino

--- cliente.ino
+++ cliente.ino
@@ -2,35 +2,29 @@
 #include <WebServer.h>
 #include <HTTPClient.h>
 
-// Configuración de red WiFi
-const char* ssid = "Cudy-A8EC";
-const char* password = "15092717";
+const char* ssid = "XXXX";
+const char* password = "XXXX";
 
-// IP del ESP32 Servidor
-const char* servidor_ip = "192.168.10.102"; // Ajusta la IP según tu configuración
+const char* servidor_ip = "XXX.XXX.XXX.XXX"; 
 
-// Crear servidor web en el puerto 80
 WebServer server(80);
 
-// Función para manejar solicitud desde la web
 void handleRequest() {
   String mensaje = obtenerMensajeDeServidor();
   server.send(200, "text/plain", mensaje);
 }
 
-// Función para obtener mensaje del ESP32 Servidor
 String obtenerMensajeDeServidor() {
   HTTPClient http;
   String mensaje;
-
-  // Construir la URL de la solicitud al servidor
+  
   String url = String("http://") + servidor_ip + "/temperatura";
   http.begin(url);
 
-  int httpCode = http.GET(); // Hacer solicitud GET
+  int httpCode = http.GET();
 
   if (httpCode > 0) {
-    mensaje = http.getString(); // Obtener respuesta si el código es positivo
+    mensaje = http.getString(); 
     Serial.println("Mensaje del ESP32 servidor recibido con éxito");
     Serial.println("Mensaje enviado al servidor web con éxito");
   } else {
@@ -42,7 +36,6 @@ String obtenerMensajeDeServidor() {
   return mensaje;
 }
 
-// Página web con JavaScript para solicitudes dinámicas
 String paginaWeb = "<html>\
 <head>\
 <title>Medicion temperatura ESP32 SERVIDOR</title>\
@@ -75,18 +68,15 @@ void setup() {
   Serial.begin(115200);
   WiFi.begin(ssid, password);
   
-  // Esperar hasta conectar a WiFi
   while (WiFi.status() != WL_CONNECTED) {
     delay(1000);
     Serial.println("Conectando a WiFi...");
   }
-
-  // Una vez conectado, imprimir la IP asignada
+  
   Serial.println("Conectado a la red WiFi");
   Serial.print("Dirección IP del ESP32 Cliente: ");
   Serial.println(WiFi.localIP());
 
-  // Configurar rutas del servidor web
   server.on("/", handleRoot);
   server.on("/solicitar", handleRequest);
   
